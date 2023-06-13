5ce9d8194f31dea276dbac35bb22a9cd0e2544c5
gabriel.bartolace <gabriel.bartolace@castgroup.com.br>
2023-06-13 19:56:30 -0400

    criar e buscar pensamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,10 +17,14 @@ import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensam
     CabecalhoComponent,
     RodapeComponent,
     CriarPensamentoComponent,
+    ListarPensamentoComponent,
+    PensamentoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
